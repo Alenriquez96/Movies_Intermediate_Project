5123b3289d194403c0b21e50e272e6ce14d9dd3b
francospatz <francospatzdilorenzo@gmail.com>
2022-04-08 07:47:14 +0200

    Updating all

--- cypress/integration/login_test.js
+++ cypress/integration/login_test.js
@@ -1,9 +1,9 @@
 describe('Login form test with incorrect parms', () => {
     it('Login scenario', () => {
-        cy.visit(Cypress.env('URL_BASE'))
+        cy.visit('https://desolate-wildwood-99118.herokuapp.com/')
         cy.get('#login-home').click()
         cy.url().should('include', 'login')
-        cy.url().should('eq', (Cypress.env('URL_BASE') + "login"))
+        cy.url().should('eq', "https://desolate-wildwood-99118.herokuapp.com/login")
         cy.get('[type="text"]').type("testuser@gmail.com")
         cy.get('[type="password"]').type("aslkjf32--gre23")
         cy.get('form').submit()
